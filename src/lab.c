46a7bc532e0a11e43194c8604dbd1dba6e5ed14f
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2024-10-17 18:34:04 +0000

    Setup

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,29 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+
+queue_t queue_init(int capacity) {
+    return NULL;
+}
+
+void queue_destroy(queue_t q) {
+
+}
+
+void enqueue(queue_t q, void *data) {
+
+}
+
+void *dequeue(queue_t q) {
+
+}
+
+void queue_shutdown(queue_t q) {
+
+}
+
+bool is_empty(queue_t q) {
+    return false;
+}
+
+bool is_shutdown(queue_t q) {
+    return false;
+}
